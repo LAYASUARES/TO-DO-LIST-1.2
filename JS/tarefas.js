9c2fff5c534061389bf5c30857700564619f0f7f
LAYASUARES <86082822+LAYASUARES@users.noreply.github.com>
2022-04-21 20:20:55 -0300

    Update tarefas.js

--- JS/tarefas.js
+++ JS/tarefas.js
@@ -221,7 +221,7 @@ function renderTasks(tasks) {
                     <p class="nome">${task.description}</p>
                     <p class="timestamp">Criada em: ${dataFormatada}</p>
                 </div>
-                <img onclick="alterarStatus(${task.id})" src="./assets/refresh.png" width="40em" height="40em"/>
+                <img onclick="alterarStatus(${task.id})" src="./ASSETS/refresh.png" width="40em" height="40em"/>
                 <img onclick="deleteTask(${task.id})" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABmJLR0QA/wD/AP+gvaeTAAABfUlEQVRoge2YoU4DQRCG/7mro+Wuvjg8JCXhHRC8BAmmtRCqQEIguIoqPMEhEPAKfQECDgvljuLIDYYmFO56O+zS5ch8ZsU/sztf9tYcoCiK4hNyuVl/d3gAxv6sGmY+6p6s7bk605mAyfATXEoYCVye3W8gywYAWi4ONeCBQdubW8tXZYWB0XbzHR4AWgQemBSaCcx3+AlLJkWmAn8W0SPu7wz5twb5TOe4bTxX5W9ABXxTeYGaTfNo9II0fUW0uIC42fiWJckYABBH9dy8qFeC1Q0kyRhZluH5Y9CvGTODmQvzol4JVgLMPLXmZWV5Xiah8m9ABXyjAr5RAd+ogG9UwDcq4BsV8I0K+MZKgIim1rysLM/LJFgJxFEdQUCIonpuRkQgKs6LeiVY/ZWIm43CPwqzMpPcFOkNpNYnlpNIimUCjBtR/U9gvpaUiwQ4C3sAHkUDSfYHPaFGPUmPSKB7unobvIUrIJzD7eeUgvmCQl7vHLbvHO6rKP+ed2yBftABMd1OAAAAAElFTkSuQmCC" />
             </li>
             </div>`
